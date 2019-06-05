0a67edbe1e6feb577ef8559702c422681a1933ba
Bojana Bajovic <bokicabajovic@yahoo.com>
2019-06-05 23:47:34 +0200

    Updates

---
+++ precache-manifest.16cc42bdad1706e8105b53fa83bec21c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95be8dfbf8388ec68ec453ec8f54d508",
+    "url": "/projekat-psql/index.html"
+  },
+  {
+    "revision": "4e0128e24e4e84816212",
+    "url": "/projekat-psql/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/projekat-psql/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4e0128e24e4e84816212",
+    "url": "/projekat-psql/static/js/main.01178167.chunk.js"
+  },
+  {
+    "revision": "659f87f0d531c78437a5",
+    "url": "/projekat-psql/static/js/runtime~main.908bd9d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/projekat-psql/static/media/logo.5d5d9eef.svg"
+  }
+]);
